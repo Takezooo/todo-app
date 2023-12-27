673f4819950ed58315169db397b3894f20602c56
Dave Jornales <plusultrastral@gmail.com>
2023-12-27 19:27:42 +0800

    css fix: removed .App, added todo-input, todo-wrapper, todo-input-item, etc.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <h1>
-        My Todo List
-      </h1>
-      <div className='todo-wrapper'>
-        <div className='todo-input'>
-          <div className='todo-input-item'>
-            <label>Description</label>
-            <input type='text' placeholder='Type Task Title' />
+    <div className="App">
+      <h1>My Todo List</h1>
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type="text" placeholder="Type Task Title" />
           </div>
-          <div className='todo-input-item'>
-            <button type='button' className='primaryBtn'>Add</button>
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input type="text" placeholder="Type Task Description" />
+          </div>
+          <div className="todo-input-item">
+            <button type="button" className="primaryBtn">
+              Add
+            </button>
           </div>
         </div>
       </div>
 
-      <div className='btn-area'>
+      <div className="btn-area">
         <button>Todo</button>
         <button>Completed</button>
       </div>
 
-      <div className='todo-list'>
-        <div className='todo-list-item'>
+      <div className="todo-list">
+        <div className="todo-list-item">
           <h3>Task 1</h3>
           <p>Description</p>
         </div>
