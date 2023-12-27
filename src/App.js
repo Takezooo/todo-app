cb54c853313b0813fa2bcd78e8e5a28a73a875c6
Dave Jornales <plusultrastral@gmail.com>
2023-12-27 22:11:11 +0800

    added icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import {AiOutlineDelete} from 'react-icons/ai';
+import { LuCheck } from "react-icons/lu";
 import "./App.css";
 
 function App() {
@@ -32,6 +34,11 @@ function App() {
           <div className="todo-list-item">
             <h3>Task 1</h3>
             <p>Description</p>
+          </div>
+
+          <div>
+            <AiOutlineDelete className="delete-icon"/>
+            <LuCheck className="check-icon" />
           </div>
         </div>
       </div>
