93051d146b2645c222062000ac9df862828dfc20
Dave Jornales <plusultrastral@gmail.com>
2023-12-29 22:43:28 +0800

    clean code + comments

--- src/App.js
+++ src/App.js
@@ -15,6 +15,32 @@ function App() {
   const [enabledIndex, setEnabledIndex] = useState(null);
   const [IsEnabled, setEnabled] = useState(false);
 
+  // Add Tasks
+  const handleAddItem = () => {
+    let newTodoItem = {
+      title: newTitle,
+      description: newDescription,
+    };
+
+    if (newTitle !== "" && newDescription !== "") {
+      let updatedTodoArr = [...allTodos];
+      updatedTodoArr.push(newTodoItem);
+      setTodos(updatedTodoArr);
+      localStorage.setItem("todolist", JSON.stringify(updatedTodoArr));
+      setNewTitle("");
+      setNewDescription("");
+    } else {
+      if (newTitle === "" && newDescription !== "") {
+        alert("Please add Title to your task");
+      } else if (newTitle !== "" && newDescription === "") {
+        alert("Please add Description to your task");
+      } else {
+        alert("Please add Title and Description to your task");
+      }
+    }
+  };
+
+  // Edit Tasks
   const handleInputEnabled = (index) => {
     setEnabledIndex((prevIndex) => (prevIndex === index ? null : index));
   };
@@ -40,30 +66,7 @@ function App() {
     }
   };
 
-  const handleAddItem = () => {
-    let newTodoItem = {
-      title: newTitle,
-      description: newDescription,
-    };
-
-    if (newTitle !== "" && newDescription !== "") {
-      let updatedTodoArr = [...allTodos];
-      updatedTodoArr.push(newTodoItem);
-      setTodos(updatedTodoArr);
-      localStorage.setItem("todolist", JSON.stringify(updatedTodoArr));
-      setNewTitle("");
-      setNewDescription("");
-    } else {
-      if (newTitle === "" && newDescription !== "") {
-        alert("Please add Title to your task");
-      } else if (newTitle !== "" && newDescription === "") {
-        alert("Please add Description to your task");
-      } else {
-        alert("Please add Title and Description to your task");
-      }
-    }
-  };
-
+  // Delete Tasks
   const handleDeleteItem = (index) => {
     let removeTodo = [...allTodos];
     removeTodo.splice(index, 1);
@@ -71,6 +74,7 @@ function App() {
     localStorage.setItem("todolist", JSON.stringify(removeTodo));
   };
 
+  //Delete Completed Tasks
   const handleDeleteCompleted = (index) => {
     let removeCompleted = [...completedTasks];
     removeCompleted.splice(index, 1);
@@ -78,6 +82,13 @@ function App() {
     localStorage.setItem("completeTask", JSON.stringify(removeCompleted));
   };
 
+  //Delete all completed tasks
+  const handleClearCompletedTasks = () => {
+    setCompletedTasks([]);
+    localStorage.removeItem("completeTask");
+  };
+
+  //Mark tasks as completed
   const handleCompleted = (index) => {
     let now = new Date();
     let dd = now.getDate();
@@ -87,8 +98,7 @@ function App() {
     let m = now.getMinutes();
     let s = now.getSeconds();
 
-    let completedOn = `${mm}-${dd}-${yyyy} at ${h}:${m}:${s}`;
-    if (IsEnabled === false) {
+    let completedOn = `${mm}-${dd}-${yyyy} at ${h}:${m}:${s}`; {
       let filteredItem = {
         ...allTodos[index],
         completedOn: completedOn,
@@ -99,171 +109,166 @@ function App() {
       setCompletedTasks(updatedCompletedArr);
       handleDeleteItem(index);
       localStorage.setItem("completeTask", JSON.stringify(updatedCompletedArr));
-    } else {
-      alert("Finish Editing first!");
-    }
-  };
 
-  const handleClearCompletedTasks = () => {
-    setCompletedTasks([]);
-    localStorage.removeItem("completeTask");
-  };
+    };
 
-  useEffect(() => {
-    let savedTodo = JSON.parse(localStorage.getItem("todolist"));
-    let savedCompleted = JSON.parse(localStorage.getItem("completeTask"));
-    if (savedTodo) {
-      setTodos(savedTodo);
-    }
+    //fetch saved tasks and completed tasks from localStorage
+    useEffect(() => {
+      let savedTodo = JSON.parse(localStorage.getItem("todolist"));
+      let savedCompleted = JSON.parse(localStorage.getItem("completeTask"));
+      if (savedTodo) {
+        setTodos(savedTodo);
+      }
 
-    if (savedCompleted) {
-      setCompletedTasks(savedCompleted);
-    }
-  }, []);
+      if (savedCompleted) {
+        setCompletedTasks(savedCompleted);
+      }
+    }, []);
 
-  return (
-    <div className="App">
-      <h1>My Todo List</h1>
-      <div className="todo-wrapper">
-        <div className="todo-input">
-          <div className="todo-input-item">
-            <label>Title</label>
-            <input
-              type="text"
-              value={newTitle}
-              onChange={(e) => setNewTitle(e.target.value)}
-              placeholder="Task Title"
-            />
+    //rendering part
+    return (
+      <div className="App">
+        <h1>My Todo List</h1>
+        <div className="todo-wrapper">
+          <div className="todo-input">
+            <div className="todo-input-item">
+              <label>Title</label>
+              <input
+                type="text"
+                value={newTitle}
+                onChange={(e) => setNewTitle(e.target.value)}
+                placeholder="Task Title"
+              />
+            </div>
+            <div className="todo-input-item">
+              <label>Description</label>
+              <input
+                type="text"
+                value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+                placeholder="Task Description"
+              />
+            </div>
+            <div className="todo-input-item">
+              <button
+                type="button"
+                onClick={handleAddItem}
+                className="primaryBtn"
+              >
+                Add
+              </button>
+            </div>
           </div>
-          <div className="todo-input-item">
-            <label>Description</label>
-            <input
-              type="text"
-              value={newDescription}
-              onChange={(e) => setNewDescription(e.target.value)}
-              placeholder="Task Description"
-            />
-          </div>
-          <div className="todo-input-item">
+
+          <div className="btn-area">
             <button
-              type="button"
-              onClick={handleAddItem}
-              className="primaryBtn"
+              className={`secondaryBtn ${isCompleteScrn === false && "active"}`}
+              onClick={() => setIsCompleteScrn(false)}
             >
-              Add
+              Todo
+            </button>
+            <button
+              className={`secondaryBtn ${isCompleteScrn === true && "active"}`}
+              onClick={() => setIsCompleteScrn(true)}
+            >
+              Completed
             </button>
           </div>
-        </div>
 
-        <div className="btn-area">
-          <button
-            className={`secondaryBtn ${isCompleteScrn === false && "active"}`}
-            onClick={() => setIsCompleteScrn(false)}
-          >
-            Todo
-          </button>
-          <button
-            className={`secondaryBtn ${isCompleteScrn === true && "active"}`}
-            onClick={() => setIsCompleteScrn(true)}
-          >
-            Completed
-          </button>
-        </div>
+          <div className="todo-list">
+            {isCompleteScrn === false &&
+              allTodos.map((item, index) => {
+                return (
+                  <div className="todo-list-item" key={index}>
+                    <div>
+                      {isInputEnabled(index) ? (
+                        <>
+                          <input
+                            type="text"
+                            value={editTitle}
+                            onChange={(e) => setEditedTitle(e.target.value)}
+                            disabled={isInputEnabled}
+                            placeholder="Edit Task Title"
+                          />
+                          <input
+                            type="text"
+                            value={editDescription}
+                            onChange={(e) => setEditedDescription(e.target.value)}
+                            disabled={isInputEnabled}
+                            placeholder="Edit Task Description"
+                          />
+                        </>
+                      ) : (
+                        <>
+                          <h3>{item.title}</h3>
+                          <p>{item.description}</p>
+                        </>
+                      )}
+                    </div>
+                    <div className="icon-area">
+                      {isInputEnabled(index) ? (
+                        <>
+                          <AiOutlineDelete
+                            className="delete-icon"
+                            onClick={() => handleInputEnabled(index)}
+                          />
+                          <LuCheck
+                            className="check-icon"
+                            onClick={() => handleEdit(index)}
+                          />
+                        </>
+                      ) : (
+                        <>
+                          <AiOutlineDelete
+                            className="delete-icon"
+                            onClick={() => handleDeleteItem(index)}
+                          />
+                          <CiEdit
+                            className="edit-icon"
+                            onClick={() => handleInputEnabled(index)}
+                          />
+                          <LuCheck
+                            className="check-icon"
+                            onClick={() => handleCompleted(index)}
+                          />
+                        </>
+                      )}
+                    </div>
+                  </div>
+                );
+              })}
 
-        <div className="todo-list">
-          {isCompleteScrn === false &&
-            allTodos.map((item, index) => {
-              return (
-                <div className="todo-list-item" key={index}>
-                  <div>
-                    {isInputEnabled(index) ? (
-                      <>
-                        <input
-                          type="text"
-                          value={editTitle}
-                          onChange={(e) => setEditedTitle(e.target.value)}
-                          disabled={isInputEnabled}
-                          placeholder="Edit Task Title"
-                        />
-                        <input
-                          type="text"
-                          value={editDescription}
-                          onChange={(e) => setEditedDescription(e.target.value)}
-                          disabled={isInputEnabled}
-                          placeholder="Edit Task Description"
-                        />
-                      </>
-                    ) : (
-                      <>
-                        <h3>{item.title}</h3>
-                        <p>{item.description}</p>
-                      </>
-                    )}
-                  </div>
-                  <div className="icon-area">
-                    {isInputEnabled(index) ? (
-                      <>
-                        <AiOutlineDelete
-                          className="delete-icon"
-                          onClick={() => handleInputEnabled(index)}
-                        />
-                        <LuCheck
-                          className="check-icon"
-                          onClick={() => handleEdit(index)}
-                        />
-                      </>
-                    ) : (
-                      <>
-                        <AiOutlineDelete
-                          className="delete-icon"
-                          onClick={() => handleDeleteItem(index)}
-                        />
-                        <CiEdit
-                          className="edit-icon"
-                          onClick={() => handleInputEnabled(index)}
-                        />
-                        <LuCheck
-                          className="check-icon"
-                          onClick={() => handleCompleted(index)}
-                        />
-                      </>
-                    )}
-                  </div>
-                </div>
-              );
-            })}
+            <button
+              className={`clearAll ${isCompleteScrn === true && "show"}`}
+              onClick={handleClearCompletedTasks}
+            >
+              Clear Completed{" "}
+            </button>
 
-          <button
-            className={`clearAll ${isCompleteScrn === true && "show"}`}
-            onClick={handleClearCompletedTasks}
-          >
-            Clear Completed{" "}
-          </button>
-
-          {isCompleteScrn === true &&
-            completedTasks.map((item, index) => {
-              return (
-                <div className="todo-list-item" key={index}>
-                  <div>
-                    <h3>{item.title}</h3>
-                    <p>{item.description}</p>
-                    <p>
-                      <small>Completed On: {item.completedOn}</small>
-                    </p>
+            {isCompleteScrn === true &&
+              completedTasks.map((item, index) => {
+                return (
+                  <div className="todo-list-item" key={index}>
+                    <div>
+                      <h3>{item.title}</h3>
+                      <p>{item.description}</p>
+                      <p>
+                        <small>Completed On: {item.completedOn}</small>
+                      </p>
+                    </div>
+                    <div className="icon-area">
+                      <AiOutlineDelete
+                        className="delete-icon"
+                        onClick={() => handleDeleteCompleted(index)}
+                      />
+                    </div>
                   </div>
-                  <div className="icon-area">
-                    <AiOutlineDelete
-                      className="delete-icon"
-                      onClick={() => handleDeleteCompleted(index)}
-                    />
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default App;
+  export default App;
