aa1c28bb9342075d28cd202bfb9389818adb93f1
Dave Jornales <plusultrastral@gmail.com>
2023-12-29 22:48:34 +0800

    error fixes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
   const [editDescription, setEditedDescription] = useState("");
   const [completedTasks, setCompletedTasks] = useState([]);
   const [enabledIndex, setEnabledIndex] = useState(null);
-  const [IsEnabled, setEnabled] = useState(false);
 
   // Add Tasks
   const handleAddItem = () => {
@@ -98,7 +97,8 @@ function App() {
     let m = now.getMinutes();
     let s = now.getSeconds();
 
-    let completedOn = `${mm}-${dd}-${yyyy} at ${h}:${m}:${s}`; {
+    let completedOn = `${mm}-${dd}-${yyyy} at ${h}:${m}:${s}`;
+    {
       let filteredItem = {
         ...allTodos[index],
         completedOn: completedOn,
@@ -109,166 +109,166 @@ function App() {
       setCompletedTasks(updatedCompletedArr);
       handleDeleteItem(index);
       localStorage.setItem("completeTask", JSON.stringify(updatedCompletedArr));
+    }
+  };
 
-    };
+  //fetch saved tasks and completed tasks from localStorage
+  useEffect(() => {
+    let savedTodo = JSON.parse(localStorage.getItem("todolist"));
+    let savedCompleted = JSON.parse(localStorage.getItem("completeTask"));
+    if (savedTodo) {
+      setTodos(savedTodo);
+    }
 
-    //fetch saved tasks and completed tasks from localStorage
-    useEffect(() => {
-      let savedTodo = JSON.parse(localStorage.getItem("todolist"));
-      let savedCompleted = JSON.parse(localStorage.getItem("completeTask"));
-      if (savedTodo) {
-        setTodos(savedTodo);
-      }
+    if (savedCompleted) {
+      setCompletedTasks(savedCompleted);
+    }
+  }, []);
 
-      if (savedCompleted) {
-        setCompletedTasks(savedCompleted);
-      }
-    }, []);
-
-    //rendering part
-    return (
-      <div className="App">
-        <h1>My Todo List</h1>
-        <div className="todo-wrapper">
-          <div className="todo-input">
-            <div className="todo-input-item">
-              <label>Title</label>
-              <input
-                type="text"
-                value={newTitle}
-                onChange={(e) => setNewTitle(e.target.value)}
-                placeholder="Task Title"
-              />
-            </div>
-            <div className="todo-input-item">
-              <label>Description</label>
-              <input
-                type="text"
-                value={newDescription}
-                onChange={(e) => setNewDescription(e.target.value)}
-                placeholder="Task Description"
-              />
-            </div>
-            <div className="todo-input-item">
-              <button
-                type="button"
-                onClick={handleAddItem}
-                className="primaryBtn"
-              >
-                Add
-              </button>
-            </div>
+  //rendering part
+  return (
+    <div className="App">
+      <h1>My Todo List</h1>
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input
+              type="text"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              placeholder="Task Title"
+            />
           </div>
-
-          <div className="btn-area">
-            <button
-              className={`secondaryBtn ${isCompleteScrn === false && "active"}`}
-              onClick={() => setIsCompleteScrn(false)}
-            >
-              Todo
-            </button>
-            <button
-              className={`secondaryBtn ${isCompleteScrn === true && "active"}`}
-              onClick={() => setIsCompleteScrn(true)}
-            >
-              Completed
-            </button>
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input
+              type="text"
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
+              placeholder="Task Description"
+            />
           </div>
-
-          <div className="todo-list">
-            {isCompleteScrn === false &&
-              allTodos.map((item, index) => {
-                return (
-                  <div className="todo-list-item" key={index}>
-                    <div>
-                      {isInputEnabled(index) ? (
-                        <>
-                          <input
-                            type="text"
-                            value={editTitle}
-                            onChange={(e) => setEditedTitle(e.target.value)}
-                            disabled={isInputEnabled}
-                            placeholder="Edit Task Title"
-                          />
-                          <input
-                            type="text"
-                            value={editDescription}
-                            onChange={(e) => setEditedDescription(e.target.value)}
-                            disabled={isInputEnabled}
-                            placeholder="Edit Task Description"
-                          />
-                        </>
-                      ) : (
-                        <>
-                          <h3>{item.title}</h3>
-                          <p>{item.description}</p>
-                        </>
-                      )}
-                    </div>
-                    <div className="icon-area">
-                      {isInputEnabled(index) ? (
-                        <>
-                          <AiOutlineDelete
-                            className="delete-icon"
-                            onClick={() => handleInputEnabled(index)}
-                          />
-                          <LuCheck
-                            className="check-icon"
-                            onClick={() => handleEdit(index)}
-                          />
-                        </>
-                      ) : (
-                        <>
-                          <AiOutlineDelete
-                            className="delete-icon"
-                            onClick={() => handleDeleteItem(index)}
-                          />
-                          <CiEdit
-                            className="edit-icon"
-                            onClick={() => handleInputEnabled(index)}
-                          />
-                          <LuCheck
-                            className="check-icon"
-                            onClick={() => handleCompleted(index)}
-                          />
-                        </>
-                      )}
-                    </div>
-                  </div>
-                );
-              })}
-
+          <div className="todo-input-item">
             <button
-              className={`clearAll ${isCompleteScrn === true && "show"}`}
-              onClick={handleClearCompletedTasks}
+              type="button"
+              onClick={handleAddItem}
+              className="primaryBtn"
             >
-              Clear Completed{" "}
+              Add
             </button>
-
-            {isCompleteScrn === true &&
-              completedTasks.map((item, index) => {
-                return (
-                  <div className="todo-list-item" key={index}>
-                    <div>
-                      <h3>{item.title}</h3>
-                      <p>{item.description}</p>
-                      <p>
-                        <small>Completed On: {item.completedOn}</small>
-                      </p>
-                    </div>
-                    <div className="icon-area">
-                      <AiOutlineDelete
-                        className="delete-icon"
-                        onClick={() => handleDeleteCompleted(index)}
-                      />
-                    </div>
-                  </div>
-                );
-              })}
           </div>
         </div>
-      </div>
-    );
-  }
 
-  export default App;
+        <div className="btn-area">
+          <button
+            className={`secondaryBtn ${isCompleteScrn === false && "active"}`}
+            onClick={() => setIsCompleteScrn(false)}
+          >
+            Todo
+          </button>
+          <button
+            className={`secondaryBtn ${isCompleteScrn === true && "active"}`}
+            onClick={() => setIsCompleteScrn(true)}
+          >
+            Completed
+          </button>
+        </div>
+
+        <div className="todo-list">
+          {isCompleteScrn === false &&
+            allTodos.map((item, index) => {
+              return (
+                <div className="todo-list-item" key={index}>
+                  <div>
+                    {isInputEnabled(index) ? (
+                      <>
+                        <input
+                          type="text"
+                          value={editTitle}
+                          onChange={(e) => setEditedTitle(e.target.value)}
+                          disabled={isInputEnabled}
+                          placeholder="Edit Task Title"
+                        />
+                        <input
+                          type="text"
+                          value={editDescription}
+                          onChange={(e) => setEditedDescription(e.target.value)}
+                          disabled={isInputEnabled}
+                          placeholder="Edit Task Description"
+                        />
+                      </>
+                    ) : (
+                      <>
+                        <h3>{item.title}</h3>
+                        <p>{item.description}</p>
+                      </>
+                    )}
+                  </div>
+                  <div className="icon-area">
+                    {isInputEnabled(index) ? (
+                      <>
+                        <AiOutlineDelete
+                          className="delete-icon"
+                          onClick={() => handleInputEnabled(index)}
+                        />
+                        <LuCheck
+                          className="check-icon"
+                          onClick={() => handleEdit(index)}
+                        />
+                      </>
+                    ) : (
+                      <>
+                        <AiOutlineDelete
+                          className="delete-icon"
+                          onClick={() => handleDeleteItem(index)}
+                        />
+                        <CiEdit
+                          className="edit-icon"
+                          onClick={() => handleInputEnabled(index)}
+                        />
+                        <LuCheck
+                          className="check-icon"
+                          onClick={() => handleCompleted(index)}
+                        />
+                      </>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+
+          <button
+            className={`clearAll ${isCompleteScrn === true && "show"}`}
+            onClick={handleClearCompletedTasks}
+          >
+            Clear Completed{" "}
+          </button>
+
+          {isCompleteScrn === true &&
+            completedTasks.map((item, index) => {
+              return (
+                <div className="todo-list-item" key={index}>
+                  <div>
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                    <p>
+                      <small>Completed On: {item.completedOn}</small>
+                    </p>
+                  </div>
+                  <div className="icon-area">
+                    <AiOutlineDelete
+                      className="delete-icon"
+                      onClick={() => handleDeleteCompleted(index)}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
